2919ea8bb66a710520de64462297d445c8e1c2ec
Santiago Cuenya <scuenya@Santi-MBPro.local>
2022-10-18 13:06:51 -0300

    3rd commit

--- app.py
+++ app.py
@@ -51,26 +51,26 @@ else:
 # Saco los DATOS
 if uploaded_file is not None:
     # Tries
-    df_triesA = df[(df['Row Name']=='TRY') & (df["EQUIPO"] == 'EQUIPO A')]
+    df_triesA = df[(df['Row Name']=='TRY') & (df["EQUIPO"] == list_of_teams[1])]
     if len( df_triesA) != 0:
         triesA = list(df_triesA["Row Name"].value_counts())
     else:
         triesA = 0
    
-    df_triesB = df[(df['Row Name']=='TRY') & (df["EQUIPO"] == 'EQUIPO B')]
+    df_triesB = df[(df['Row Name']=='TRY') & (df["EQUIPO"] == list_of_teams[2])]
     if len(df_triesB) != 0:
         triesB = list(df_triesB["Row Name"].value_counts())
     else:
         triesB = 0
    
     # PENALTY TRIES
-    df_p_triesA = df[(df['Row Name']=='PENALTY TRY') & (df["EQUIPO"] == 'EQUIPO A')]
+    df_p_triesA = df[(df['Row Name']=='PENALTY TRY') & (df["EQUIPO"] == list_of_teams[1])]
     if len(df_p_triesA) != 0  :
         p_triesA = list(df_p_triesA["Row Name"].value_counts())
     else:
         p_triesA = [0]
     
-    df_p_triesB = df[(df['Row Name']=='PENALTY TRY') & (df["EQUIPO"] == 'EQUIPO B')]
+    df_p_triesB = df[(df['Row Name']=='PENALTY TRY') & (df["EQUIPO"] == list_of_teams[2])]
    
     if len(df_p_triesB) != 0  :
         p_triesB = list(df_p_triesB["Row Name"].value_counts())
@@ -78,52 +78,52 @@ if uploaded_file is not None:
         p_triesB = [0]
 
     # GOALS SUCCESS
-    df_goalsA = df[(df['Row Name']=='GOAL') & (df["EQUIPO"] == 'EQUIPO A') & (df["RESULTADO"] == 'CONVERTIDO')]
+    df_goalsA = df[(df['Row Name']=='GOAL') & (df["EQUIPO"] == list_of_teams[1]) & (df["RESULTADO"] == 'CONVERTIDO')]
     if len(df_goalsA) != 0  :
         goalsA = list(df_goalsA["Row Name"].value_counts())
     else:
         goalsA = [0]
     
-    df_goalsB = df[(df['Row Name']=='GOAL') & (df["EQUIPO"] == 'EQUIPO B') & (df["RESULTADO"] == 'CONVERTIDO')]
+    df_goalsB = df[(df['Row Name']=='GOAL') & (df["EQUIPO"] == list_of_teams[2]) & (df["RESULTADO"] == 'CONVERTIDO')]
     if len(df_goalsB) != 0  :
         goalsB = list(df_goalsB["Row Name"].value_counts())
     else:
         goalsB = [0]
 
    # GOALS ERROR
-    df_no_goalsA = df[(df['Row Name']=='GOAL') & (df["EQUIPO"] == 'EQUIPO A') & (df["RESULTADO"] == 'ERRADO')]
+    df_no_goalsA = df[(df['Row Name']=='GOAL') & (df["EQUIPO"] == list_of_teams[1]) & (df["RESULTADO"] == 'ERRADO')]
     if len(df_no_goalsA) != 0:
         no_goalsA = list(df_no_goalsA["Row Name"].value_counts())
     else:
         no_goalsA = [0]
     
-    df_no_goalsB = df[(df['Row Name']=='GOAL') & (df["EQUIPO"] == 'EQUIPO B') & (df["RESULTADO"] == 'ERRADO')]
+    df_no_goalsB = df[(df['Row Name']=='GOAL') & (df["EQUIPO"] == list_of_teams[2]) & (df["RESULTADO"] == 'ERRADO')]
     if len(df_no_goalsB) != 0  :
         no_goalsB = list(df_no_goalsB["Row Name"].value_counts())
     else:
         no_goalsB = [0]
 
     # PENALTY KICK SUCCESS
-    df_pen_KickA = df[(df['Row Name']=='PENALTY KICK') & (df["EQUIPO"] == 'EQUIPO A') & (df["RESULTADO"] == 'CONVERTIDO')]
+    df_pen_KickA = df[(df['Row Name']=='PENALTY KICK') & (df["EQUIPO"] == list_of_teams[1]) & (df["RESULTADO"] == 'CONVERTIDO')]
     if len(df_pen_KickA) != 0  :
         pen_KickA = list(df_pen_KickA["Row Name"].value_counts())
     else:
         pen_KickA = [0]
     
-    df_pen_KickB = df[(df['Row Name']=='PENALTY KICK') & (df["EQUIPO"] == 'EQUIPO A') & (df["RESULTADO"] == 'CONVERTIDO')]
+    df_pen_KickB = df[(df['Row Name']=='PENALTY KICK') & (df["EQUIPO"] == list_of_teams[1]) & (df["RESULTADO"] == 'CONVERTIDO')]
     if len(df_pen_KickB) != 0  :
         pen_KickB = list(df_pen_KickB["Row Name"].value_counts())
     else:
         pen_KickB = [0]
 
     # PENALTY KICK ERRORS
-    df_no_pen_KickA = df[(df['Row Name']=='PENALTY KICK') & (df["EQUIPO"] == 'EQUIPO A') & (df["RESULTADO"] == 'ERRADO')]
+    df_no_pen_KickA = df[(df['Row Name']=='PENALTY KICK') & (df["EQUIPO"] == list_of_teams[1]) & (df["RESULTADO"] == 'ERRADO')]
     if len(df_no_pen_KickA) != 0  :
         no_pen_KickA = list(df_no_pen_KickA["Row Name"].value_counts())
     else:
         no_pen_KickA = [0]
     
-    df_no_pen_KickB = df[(df['Row Name']=='PENALTY KICK') & (df["EQUIPO"] == 'EQUIPO B') & (df["RESULTADO"] == 'ERRADO')]
+    df_no_pen_KickB = df[(df['Row Name']=='PENALTY KICK') & (df["EQUIPO"] == list_of_teams[2]) & (df["RESULTADO"] == 'ERRADO')]
     if len(df_no_pen_KickB) != 0  :
         no_pen_KickB = list(df_no_pen_KickB["Row Name"].value_counts())
     else:
@@ -131,52 +131,52 @@ if uploaded_file is not None:
 
 
     # DROP GOALS
-    df_drop_goalsA = df[(df['Row Name']=='DROP') & (df["EQUIPO"] == 'EQUIPO A') & (df["RESULTADO"] == 'CONVERTIDO')]
+    df_drop_goalsA = df[(df['Row Name']=='DROP') & (df["EQUIPO"] == list_of_teams[1]) & (df["RESULTADO"] == 'CONVERTIDO')]
     if len(df_drop_goalsA) != 0  :
         drop_goalsA = list(df_drop_goalsA["Row Name"].value_counts())
     else:
         drop_goalsA = [0]
     
-    df_drop_goalsB = df[(df['Row Name']=='DROP') & (df["EQUIPO"] == 'EQUIPO B') & (df["RESULTADO"] == 'CONVERTIDO')]
+    df_drop_goalsB = df[(df['Row Name']=='DROP') & (df["EQUIPO"] == list_of_teams[2]) & (df["RESULTADO"] == 'CONVERTIDO')]
     if len(df_drop_goalsB) != 0  :
         drop_goalsB = list(df_drop_goalsB["Row Name"].value_counts())
     else:
         drop_goalsB = [0]
 
 # DROP GOALS ERRADO
-    df_no_drop_goalsA = df[(df['Row Name']=='DROP') & (df["EQUIPO"] == 'EQUIPO A') & (df["RESULTADO"] == 'ERRADO')]
+    df_no_drop_goalsA = df[(df['Row Name']=='DROP') & (df["EQUIPO"] == list_of_teams[1]) & (df["RESULTADO"] == 'ERRADO')]
     if len(df_no_drop_goalsA) != 0  :
         no_drop_goalsA = list(df_no_drop_goalsA["Row Name"].value_counts())
     else:
         no_drop_goalsA = [0]
     
-    df_no_drop_goalsB = df[(df['Row Name']=='DROP') & (df["EQUIPO"] == 'EQUIPO B') & (df["RESULTADO"] == 'ERRADO')]
+    df_no_drop_goalsB = df[(df['Row Name']=='DROP') & (df["EQUIPO"] == list_of_teams[2]) & (df["RESULTADO"] == 'ERRADO')]
     if len(df_no_drop_goalsB) != 0  :
         no_drop_goalsB = list(df_no_drop_goalsB["Row Name"].value_counts())
     else:
         no_drop_goalsB = [0]
 
     # YELLOW CARD
-    df_yellow_cardA = df[(df['Row Name']=='YELLOW CARD') & (df["EQUIPO"] == 'EQUIPO A')]
+    df_yellow_cardA = df[(df['Row Name']=='YELLOW CARD') & (df["EQUIPO"] == list_of_teams[1])]
     if len( df_yellow_cardA) != 0:
         yellow_cardA = list(df_yellow_cardA["Row Name"].value_counts())
     else:
         yellow_cardA = [0]
    
-    df_yellow_cardB = df[(df['Row Name']=='YELLOW CARD') & (df["EQUIPO"] == 'EQUIPO B')]
+    df_yellow_cardB = df[(df['Row Name']=='YELLOW CARD') & (df["EQUIPO"] == list_of_teams[2])]
     if len(df_yellow_cardB) != 0:
         yellow_cardB = list(df_yellow_cardB["Row Name"].value_counts())
     else:
         yellow_cardB = [0]
 
     # RED CARD
-    df_red_cardA = df[(df['Row Name']=='RED CARD') & (df["EQUIPO"] == 'EQUIPO A')]
+    df_red_cardA = df[(df['Row Name']=='RED CARD') & (df["EQUIPO"] == list_of_teams[1])]
     if len(df_red_cardA) != 0:
         red_cardA = list(df_red_cardA["Row Name"].value_counts())
     else:
         red_cardA = [0]
    
-    df_red_cardB = df[(df['Row Name']=='RED CARD') & (df["EQUIPO"] == 'EQUIPO B')]
+    df_red_cardB = df[(df['Row Name']=='RED CARD') & (df["EQUIPO"] == list_of_teams[2])]
     if len(df_red_cardB) != 0:
         red_cardB = list(df_red_cardB["Row Name"].value_counts())
     else:
@@ -185,25 +185,25 @@ if uploaded_file is not None:
 
 
     # SCRUMS GANADOS
-    df_scrumA = df[(df['Row Name']=='SCRUM') & (df["EQUIPO"] == 'EQUIPO A') & (df["RESULTADO"]== 'GANADO')]
+    df_scrumA = df[(df['Row Name']=='SCRUM') & (df["EQUIPO"] == list_of_teams[1]) & (df["RESULTADO"]== 'GANADO')]
     if len(df_scrumA) != 0:
         scrumA = list(df_scrumA["Row Name"].value_counts())
     else:
         scrumA = [0]
 
-    df_scrumB = df[(df['Row Name']=='SCRUM') & (df["EQUIPO"] == 'EQUIPO B') & (df["RESULTADO"]== 'GANADO')]
+    df_scrumB = df[(df['Row Name']=='SCRUM') & (df["EQUIPO"] == list_of_teams[2]) & (df["RESULTADO"]== 'GANADO')]
     if len(df_scrumB) != 0:
         scrumB = list(df_scrumB["Row Name"].value_counts())
     else:
         scrumB = [0]
 
     # LINEOUT GANADOS
-    df_lineoutA = df[(df['Row Name']=='LINEOUT') & (df["EQUIPO"] == 'EQUIPO A') & (df["RESULTADO"]== 'GANADO')]
+    df_lineoutA = df[(df['Row Name']=='LINEOUT') & (df["EQUIPO"] == list_of_teams[1]) & (df["RESULTADO"]== 'GANADO')]
     if len(df_lineoutA) != 0:
         lineoutA = list(df_lineoutA["Row Name"].value_counts())
     else:
         lienoutA = [0]
-    df_lineoutB = df[(df['Row Name']=='LINEOUT') & (df["EQUIPO"] == 'EQUIPO B') & (df["RESULTADO"]== 'GANADO')]
+    df_lineoutB = df[(df['Row Name']=='LINEOUT') & (df["EQUIPO"] == list_of_teams[2]) & (df["RESULTADO"]== 'GANADO')]
     if len(df_lineoutB) != 0:
         lineoutB = list(df_lineoutB["Row Name"].value_counts())
     else:
